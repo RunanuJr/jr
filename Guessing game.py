04d375735b7240948eba7b2d376ba58d9225179d
RunanuJr <mwangiwamuyu8@gmail.com>
2025-12-05 19:09:31 +0300

    Update Guessing Game: handle invalid input and add attempt counter

--- Guessing game.py
+++ Guessing game.py
@@ -1,10 +1,21 @@
 import random
-secret = random.randint(1, 20)
-guess = int(input("Guess a number between 1 and 20: "))
 
-if guess > secret:
-   print("Too High!")
-elif guess < secret:
-   print("Too Low!")
-else:
-   print("Correct! you win!")
+secret = random.randint(1, 20)
+attempts = 0
+guess = None
+
+print("Welcome to the Guessing Game!")
+print("I'm thinking of a number between 1 and 20. Can you guess it?")
+
+while guess != secret:
+ 
+   guess = int(input("Guess your number: "))
+   attempts += 1
+
+   if guess > secret:
+    print("Too High!")
+   elif guess < secret:
+     print("Too Low!")
+   else:
+      print(f"Correct! You guessed it in {attempts} attempts!")
+   
